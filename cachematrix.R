8aa2230a8ad97a48d9314a94ce3566732f14e92c
jjsiv4 <jjsiv4@gmail.com>
2015-06-19 21:22:59 -0400

    Added comments and minor mod return value to cachesolve() function

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,10 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Function makeCacheMatrix
+## Usage: Assign function to var - x <- makeCacheMatrix()
+##        Then use setter to assign matrix -  x$set(matrix(1:4,2,2))
+##        To check value was assigned use get - x$get()
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -20,15 +23,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## Function cachesolve
+## Will use function assigned to function parm to retrieve matrix and assign to m
+## Then Test if m is not empty and unchanged then will return cached value
+##
+## Otherwise calculates inverse using solve(), sets cache and returns matrix
 cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getmatrix()
-        if(!is.null(m) || identical(m, x)) {
+        
+        ## Testing if matrix exists and it has not changed.  If true, return cache
+        if(!is.null(m) && identical(m, x)) {
             message("getting cached data")
             return(m)
         }
+        
+        ## Test failed, use solve to calculate inverse of matrix and set
         matrix <- x$get()
         m <- solve(matrix, ...)
         x$setmatrix(m)
+        m
 }
